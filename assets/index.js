40369594494b22b36d07f302cc164536bccc0847
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-03-21 01:39:58 +0600

    first div done adding

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,2 @@
 export { default as PersonImage } from "./images/hero.png";
+export { default as Avater } from "./images/avatar.png";
--- screens/Discover.js
+++ screens/Discover.js
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Avater } from "../assets";
 
 const Discover = () => {
-  return (
-    <View>
-      <Text>Discover</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
-export default Discover
+  return (
+    <SafeAreaView>
+      <View className="flex-1 flex-row justify-between px-8 mt-5">
+        <View className="flex-col">
+          <Text className="text-3xl font-bold text-[#0B646B]">Discover</Text>
+          <Text className="text-3xl text-[#527283]">the beauty today</Text>
+        </View>
+        <View>
+          <Image 
+          source={Avater}
+          className="w-16 h-16 rounded-xl"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Discover;
